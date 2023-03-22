e37f537ec6d95a71f2d06c6803843fef43f67c1d
ShweMoeThantAurum <shwemoethant@my.jcu.edu.au>
2023-03-22 14:12:01 +0800

    Creat a for loop to print the wanted asterisks

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,3 +2,5 @@ MINIMUM_PASSWORD_LENGTH = 8
 password = input("Enter your password: ")
 while len(password) < MINIMUM_PASSWORD_LENGTH:
     password = input("Enter your password: ")
+for i in range(len(password)):
+    print("*", end=" ")
