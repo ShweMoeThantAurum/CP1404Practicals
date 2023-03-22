9d33bc68f457844ab9e56290e4450ece0343ec03
ShweMoeThantAurum <shwemoethant@my.jcu.edu.au>
2023-03-22 21:35:43 +0800

    Add main function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,28 +1,32 @@
-MENU = "(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "G":
-        score = int(input("Score: "))
-        while score < 0 or score > 100:
-            print("Invalid score")
-            score = int(input("Score: "))
-    elif choice == "P":
-        if score < 0 or score > 100:
-            message = "Invalid score"
-        elif score >= 90:
-            message = "Excellent"
-        elif score >= 50:
-            message = "Passable"
-        else:
-            message = "Bad"
-        print(message)
-    elif choice == "S":
-        for i in range(score):
-            print("*" * i)
-    else:
-        print("Invalid choice")
+def main():
+    MENU = "(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
     print(MENU)
     choice = input(">>> ").upper()
-print("Farewell")
+    while choice != "Q":
+        if choice == "G":
+            score = int(input("Score: "))
+            while score < 0 or score > 100:
+                print("Invalid score")
+                score = int(input("Score: "))
+        elif choice == "P":
+            if score < 0 or score > 100:
+                message = "Invalid score"
+            elif score >= 90:
+                message = "Excellent"
+            elif score >= 50:
+                message = "Passable"
+            else:
+                message = "Bad"
+            print(message)
+        elif choice == "S":
+            for i in range(score):
+                print("*" * i)
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Farewell")
+
+
+main()
 
