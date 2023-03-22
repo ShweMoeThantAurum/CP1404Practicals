b4756a85bcabbf1b638ed55641553b61409f5b63
ShweMoeThantAurum <shwemoethant@my.jcu.edu.au>
2023-03-22 14:19:30 +0800

    Add main function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,10 @@
-MINIMUM_PASSWORD_LENGTH = 8
-password = input("Enter your password: ")
-while len(password) < MINIMUM_PASSWORD_LENGTH:
+def main():
+    MINIMUM_PASSWORD_LENGTH = 8
     password = input("Enter your password: ")
-for i in range(len(password)):
-    print("*", end=" ")
+    while len(password) < MINIMUM_PASSWORD_LENGTH:
+        password = input("Enter your password: ")
+    for i in range(len(password)):
+        print("*", end=" ")
+
+
+main()
