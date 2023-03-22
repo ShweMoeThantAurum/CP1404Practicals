5faf1b32f7e2bae2d4cf7354cfe1912752d2b1b9
ShweMoeThantAurum <shwemoethant@my.jcu.edu.au>
2023-03-22 14:51:50 +0800

    Add score input and print result

---
+++ prac_02/score.py
@@ -0,0 +1,10 @@
+score = float(input("Enter score: "))
+if score < 0 or score > 100:
+    message = "Invalid score"
+elif score >= 90:
+    message = "Excellent"
+elif score >= 50:
+    message = "Passable"
+else:
+    message = "Bad"
+print(message)
