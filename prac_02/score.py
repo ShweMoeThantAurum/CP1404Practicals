c9402a31971bfcead78865f7093e1bb74c2a64f8
ShweMoeThantAurum <shwemoethant@my.jcu.edu.au>
2023-03-22 15:14:55 +0800

    Update MINIMUM_SCORE AND MAXIMUM_SCORE

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,15 +1,19 @@
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
+
+
 def main():
     import random
     score = float(input("Enter score: "))
     message = get_result(score)
     print(message)
-    random_score = random.randint(0, 100)
+    random_score = random.randint(MINIMUM_SCORE, MAXIMUM_SCORE)
     score = random_score
     print(get_result(score))
 
 
 def get_result(score):
-    if score < 0 or score > 100:
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
         message = "Invalid score"
     elif score >= 90:
         message = "Excellent"
