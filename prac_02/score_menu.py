33db6014ec84662f44e49350b1057b42913998ae
ShweMoeThantAurum <shwemoethant@my.jcu.edu.au>
2023-03-22 21:36:27 +0800

    Refactor get_valid_score function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -4,10 +4,7 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "G":
-            score = int(input("Score: "))
-            while score < 0 or score > 100:
-                print("Invalid score")
-                score = int(input("Score: "))
+            score = get_valid_score()
         elif choice == "P":
             if score < 0 or score > 100:
                 message = "Invalid score"
@@ -26,6 +23,14 @@ def main():
         print(MENU)
         choice = input(">>> ").upper()
     print("Farewell")
+
+
+def get_valid_score():
+    score = int(input("Score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = int(input("Score: "))
+    return score
 
 
 main()
