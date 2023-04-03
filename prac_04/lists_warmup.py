8f3add97714997b9e26dde293bea6af507b5ce8c
ShweMoeThantAurum <shwemoethant@my.jcu.edu.au>
2023-04-03 15:41:49 +0800

    Update the file

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,2 +1,9 @@
 numbers = ["ten", 1, 4, 1, 5, 9, 1]
+# Print all the elements from numbers except the first two (slice)
 print(numbers[2:])
+# Print whether 9 is an element of numbers
+if 9 in numbers:
+    message = "9 is an element of numbers"
+else:
+    message = "9 is not an element of numbers"
+print(message)
