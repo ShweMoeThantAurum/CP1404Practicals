5eff1bfdfa77f433d000c8f786e077eadb4a5be8
ShweMoeThantAurum <shwemoethant@my.jcu.edu.au>
2023-04-12 12:01:55 +0800

    Update the file

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,7 @@
+string = input("Text: ")
+word_to_count = {}
+for word in list(sorted(string.split())):
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+max_length = max(len(word) for word in list(word_to_count.keys()))
+for word, count in word_to_count.items():
+    print(f"{word:{max_length}} : {count}")
