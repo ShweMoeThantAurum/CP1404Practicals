df9be51bc4a655b045f344b8539db367dc8160b7
ShweMoeThantAurum <shwemoethant@my.jcu.edu.au>
2023-04-26 10:48:33 +0800

    Make init function

---
+++ prac_06/programming_language.py
@@ -0,0 +1,6 @@
+class ProgrammingLanguage:
+    def __init__(self, language_name, typing, reflection, year=0):
+        self.language_name = language_name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
