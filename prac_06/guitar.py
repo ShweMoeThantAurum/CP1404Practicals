93bab8a5525e88e6f99f4b78dccf579cbe0cca76
ShweMoeThantAurum <shwemoethant@my.jcu.edu.au>
2023-04-30 09:58:49 +0800

    Update the file

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,7 +1,10 @@
 CURRENT_YEAR = 2022
 VINTAGE_AGE = 50
+
+
 class Guitar:
     """Represent information about guitar """
+
     def __init__(self, name, year=0, cost=0):
         """Create a Guitar from the given values"""
         self.name = name
@@ -12,9 +15,13 @@ class Guitar:
         """Return string representation of a Guitar."""
         return f"{self.name} ({self.year}) : ${self.cost}"
 
+    def __repr__(self):
+        """Return string representation of a Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost}"
+
     def get_age(self):
         """Calculate how old the guitar is in years."""
-        return CURRENT_YEAR-self.year
+        return CURRENT_YEAR - self.year
 
     def is_vintage(self):
         """Determine whether a guitar is vintage or not."""
@@ -22,3 +29,7 @@ class Guitar:
             return True
         else:
             return False
+
+    def __lt__(self, other):
+        "Sort the list of guitars by released year."
+        return self.year < other.year
